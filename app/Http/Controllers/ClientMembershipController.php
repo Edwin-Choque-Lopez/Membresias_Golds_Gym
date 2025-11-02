c91b8b70a43f423e93ee4ef0f6b335cb4a043f6e
Edwin Choque <edwin@gmail.com>
2025-11-01 21:16:29 -0400

    Creación de los Modelo, Migraciones, Controladores y Factories. Para la base de datos

---
+++ app/Http/Controllers/ClientMembershipController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client_Membership;
+use Illuminate\Http\Request;
+
+class ClientMembershipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client_Membership $client_Membership)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client_Membership $client_Membership)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Client_Membership $client_Membership)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client_Membership $client_Membership)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PaymentStatuseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment_Statuse;
+use Illuminate\Http\Request;
+
+class PaymentStatuseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Payment_Statuse $payment_Statuse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Payment_Statuse $payment_Statuse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Payment_Statuse $payment_Statuse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Payment_Statuse $payment_Statuse)
+    {
+        //
+    }
+}
